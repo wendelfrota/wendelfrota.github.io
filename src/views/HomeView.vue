4e2c2605a64a4bb7e569b6cdee716e12d6e59650
wendelfrota <wendelalves898@gmail.com>
2024-08-26 20:41:11 -0300

    add: home view

---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script setup>
+import NavComponent from '../components/NavComponent.vue'
+import HomeComponent from '@/components/HomeComponent.vue';
+import ProjectsComponent from '@/components/ProjectsComponent.vue';
+import TechnologiesComponent from '@/components/TechnologiesComponent.vue';
+import AboutComponent from '@/components/AboutComponent.vue';
+import ContactComponent from '@/components/ContactComponent.vue';
+</script>
+
+<template>
+  <header class="vertical-center">
+    <a href="#home">
+      <h1 class="logo">Wendel Frota</h1>
+    </a>
+
+    <NavComponent />
+  </header>
+  <main>
+    <HomeComponent />
+    <ProjectsComponent />
+    <TechnologiesComponent />
+    <AboutComponent />
+  </main>
+
+  <footer id="contact">
+    <ContactComponent />
+  </footer>
+</template>
