e53a234a654df5a8da87cf6e88f790bf7f8cbc4e
wendelfrota <wendelalves898@gmail.com>
2024-08-26 20:40:49 -0300

    add: initial components

---
+++ src/components/NavComponent.vue
@@ -0,0 +1,17 @@
+<script setup>
+import IconSwitchTheme from './icons/IconSwitchTheme.vue'
+</script>
+
+<template>
+    <nav>
+        <ul class="vertical-center">
+            <li><a href="#home">Início</a></li>
+            <li><a href="#projects">Projetos</a></li>
+            <li><a href="#technologies">Tecnologias</a></li>
+            <li><a href="#about">Sobre</a></li>
+            <li><a href="#contact">Contato</a></li>
+        </ul>
+    </nav>
+
+    <IconSwitchTheme />
+</template>
