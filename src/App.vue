f18878bee29745fa6cb1dcdd41d33c523b4c6ce7
wendelfrota <wendelalves898@gmail.com>
2024-08-27 14:46:43 -0300

    change: app view

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router';
+import NavComponent from '@/components/NavComponent.vue'
+import ContactComponent from '@/components/ContactComponent.vue';
 </script>
 
 <template>
-  <RouterLink to="/" />
-  <RouterView />
+  <header class="vertical-center">
+    <a href="#home">
+      <h1 class="logo">Wendel Frota</h1>
+    </a>
+
+    <NavComponent />
+  </header>
+  <main>
+    <RouterView />
+  </main>
+
+  <footer id="contact">
+    <ContactComponent />
+  </footer>
 </template>
