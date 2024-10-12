091979bab336c947987fce262ed8b200b4a70d61
wendelfrota <wendelalves898@gmail.com>
2024-10-12 13:02:08 -0300

    add: header component

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,37 @@
+<script setup>
+import NavComponent from '@/components/NavComponent.vue'
+</script>
+
+<template>
+    <header>
+        <div class="logo-container">
+            <a href="#home">
+                <img src="@/assets/img/full-logo.png" alt="Full Logo">
+            </a>
+        </div>
+
+        <NavComponent />
+    </header>
+</template>
+
+<style scoped>
+    header{
+        display: flex;
+        align-items: center;
+        padding: 20px;
+    }
+
+    img{
+        width: 100%;
+    }
+
+    .logo-container{
+        max-width: 256px;
+    }
+
+    @media (max-width: 768px) {
+        .logo-container {
+            max-width: 128px;
+        }
+    }
+</style>
