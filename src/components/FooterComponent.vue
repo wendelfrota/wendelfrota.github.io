06172aa61538783586bc76089272ffe21a563b4e
wendelfrota <wendelalves898@gmail.com>
2024-10-21 15:17:44 -0300

    add: footer component

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,15 @@
+<script setup>
+import ContactComponent from '@/components/ContactComponent.vue'
+</script>
+
+<template>
+    <footer>
+        <ContactComponent />
+    </footer>
+</template>
+
+<style scoped>
+footer {
+    height: 50vh;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HeaderComponent from '@/components/HeaderComponent.vue';
 import MainComponent from '@/components/MainComponent.vue';
+import FooterComponent from '@/components/FooterComponent.vue';
 </script>
 
 <template>
     <HeaderComponent />
     <MainComponent />
+    <FooterComponent />
 </template>
