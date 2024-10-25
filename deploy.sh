69d38ba6f850830be54c919df0e341888ba57f2b
wendelfrota <wendelalves898@gmail.com>
2024-10-25 14:27:18 -0300

    fix: correct deployment script and workflow configuration

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,12 @@ cd dist
 git init
 git add -A
 git commit -m "deploy: ($CURRENT_DATE)"
-git push git@github.com:wendelfrota/wendelfrota.github.io.git master:gh-pages
+
+if git push git@github.com:wendelfrota/wendelfrota.github.io.git master:gh-pages; then
+    echo "Push to the repository via SSH succeeded."
+else
+    echo "SSH push failed. Trying with HTTPS..."
+    git push https://x-access-token:${GITHUB_TOKEN}@github.com/wendelfrota/wendelfrota.github.io.git master:gh-pages
+fi
 
 cd -
